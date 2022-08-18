665137554537e7f24be07c63f46f2438adf23545
kevinzepwa <zepwakevin8@gmail.com>
2022-08-18 16:35:14 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+function writeCards(friends, event) {
+  let arrayNames = [] 
+  for (let i = 0; i < friends.length; i++) {
+    arrayNames.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+  }
+  return arrayNames
+}
+
+function countDown(positiveInteger) { 
+    while (positiveInteger > -1) {
+      console.log(positiveInteger--);
+    }
+}
+
+writeCards(["friends", "good"], "event")
